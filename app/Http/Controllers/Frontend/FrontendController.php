f21e4e8b6e35f123dadaa7a8154ad20ee61b49c4
unknown <rafalficek082@gmail.com>
2024-05-06 08:27:08 +0200

    Why choose us section

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -9,6 +9,53 @@ use Illuminate\Http\Request;
 class FrontendController extends Controller
 {
     function index() : View {
-        return view('frontend.home.index');
-    } //
+        $sectionTitles = $this->getSectionTitles();
+
+        $sliders = Slider::where('status', 1)->get();
+        $whyChooseUs = WhyChooseUs::where('status', 1)->get();
+        $categories = Category::where(['show_at_home' => 1, 'status' => 1])->get();
+        $dailyOffers = DailyOffer::with('product')->where('status', 1)->take(15)->get();
+        $bannerSliders = BannerSlider::where('status', 1)->latest()->take(10)->get();
+        $chefs = Chef::where(['show_at_home' => 1, 'status' => 1])->get();
+        $appSection = AppDownloadSection::first();
+        $testimonials = Testimonial::where(['show_at_home' => 1, 'status' => 1])->get();
+        $counter = Counter::first();
+        $latestBlogs = Blog::withCount(['comments' => function($query){
+            $query->where('status', 1);
+        }])->with(['category', 'user'])->where('status', 1)->latest()->take(3)->get();
+
+        return view('frontend.home.index',
+            compact(
+                'sliders',
+                'sectionTitles',
+                'whyChooseUs',
+                'categories',
+                'dailyOffers',
+                'bannerSliders',
+                'chefs',
+                'appSection',
+                'testimonials',
+                'counter',
+                'latestBlogs'
+            ));
+    }
+
+    function getSectionTitles() : Collection {
+        $keys = [
+            'why_choose_top_title',
+            'why_choose_main_title',
+            'why_choose_sub_title',
+            'daily_offer_top_title',
+            'daily_offer_main_title',
+            'daily_offer_sub_title',
+            'chef_top_title',
+            'chef_main_title',
+            'chef_sub_title',
+            'testimonial_top_title',
+            'testimonial_main_title',
+            'testimonial_sub_title'
+        ];
+
+        return SectionTitle::whereIn('key', $keys)->pluck('value','key');
+    }
 }
